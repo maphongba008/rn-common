e9cf955ed78eb7930004c0448b58d56be776f994
Quynh Dang Ta <maphongba008@users.noreply.github.com>
2024-07-27 16:00:18 +0700

    logger doc

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -2,8 +2,11 @@ import { fileAsyncTransport, logger as FileLogger } from "react-native-logs";
 import * as FileSystem from "expo-file-system";
 import moment from "moment";
 
+// Define the directory where logs will be stored
 const logDir = FileSystem.cacheDirectory + "logs";
 FileSystem.makeDirectoryAsync(logDir, { intermediates: true });
+
+// Define the default configuration for the logger
 const defaultConfig = {
   levels: {
     debug: 0,
@@ -25,8 +28,10 @@ const defaultConfig = {
   enabled: true,
 };
 
+// Create the file logger using the default configuration
 const fileLogger = FileLogger.createLogger(defaultConfig);
 
+// Convert the message to a string representation
 const toString = (message: any) => {
   if (typeof message === "string") {
     return message;
@@ -34,19 +39,43 @@ const toString = (message: any) => {
   return JSON.stringify(message);
 };
 
+/**
+ * The logger object provides methods for logging messages with different severity levels.
+ */
 export const logger = {
+  /**
+   * Set the severity level of the logger. 
+   * Log messages with a severity level lower than the specified level will be ignored.
+   * @param severity The severity level to set. Valid values are "debug", "info", "warn", or "error".
+   */
   setSeverity: (severity: "debug" | "info" | "warn" | "error") => {
     fileLogger.setSeverity(severity);
   },
+  /**
+   * Log a debug message.
+   * @param message The message to log.
+   */
   debug: (message: any) => {
     fileLogger.debug(toString(message));
   },
+  /**
+   * Log an info message.
+   * @param message The message to log.
+   */
   info: (message: any) => {
     fileLogger.info(toString(message));
   },
+  /**
+   * Log a warning message.
+   * @param message The message to log.
+   */
   warn: (message: any) => {
     fileLogger.warn(toString(message));
   },
+  /**
+   * Log an error message.
+   * @param message The message to log.
+   */
   error: (message: any) => {
     fileLogger.error(toString(message));
   },
