41b2f28e4469bdaa8fade6e3b32ca075be17698b
Quynh Dang Ta <maphongba008@users.noreply.github.com>
2025-05-24 10:55:42 +0700

    bump all version

--- scripts/manageDeps.js
+++ scripts/manageDeps.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const root = `${__dirname}/..`
-const sharedPackageVersion = '53.0.1'
+const sharedPackageVersion = '53.0.2'
 const srcPackages = `${root}/packages`
 
 const sharedPeerDeps = ['react-native', 'expo']
